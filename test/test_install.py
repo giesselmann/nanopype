97f337ad06b25a237b58beda04f5a24daf3bb79f
Pay Giesselmann <pay.giesselmann@web.de>
2018-11-29 19:02:30 +0100

    test presence of tools

--- test/test_install.py
+++ test/test_install.py
@@ -31,37 +31,43 @@
 #
 # Written by Pay Giesselmann
 # ---------------------------------------------------------------------------------
-import os, unittest
+import os, unittest, yaml
 
+
+def __is_exe__(fpath):
+    return os.path.isfile(fpath) and os.access(fpath, os.X_OK)
+    
+    
 # Test cases
 class test_unit_binaries(unittest.TestCase):
     def setUp(self):
-        pass
-        
+        with open(os.path.join(os.path.dirname(os.path.abspath(__file__)), '..', "env.yaml"), 'r') as fp:
+            self.nanopype_env = yaml.load(fp)
+            
     # Test if basecaller is installed
-    def test_basecalling(self):
-        pass
-
-    # Test aligner installations
-    def test_alignment(self):
-        pass
-        
-    # Test methylation detection
-    def test_methylation(self):
-        pass
-        
-    # Test structural variation detection
-    def test_sv(self):
-        pass
-        
-    # Test misc.
-    def test_misc(self):
-        pass
-
-
-        
-        
-        
+    def test_callables(self):
+        for key, ex in self.nanopype_env['bin'].items():
+            print("Testing " + str(key))
+            # from https://stackoverflow.com/questions/377017/test-if-executable-exists-in-python
+            
+            fpath, fname = os.path.split(ex)
+            if fpath:
+                if __is_exe__(ex):
+                    print(" ".join(["Found", key, "in", fpath]))
+                else:
+                    raise EnvironmentError(key + ": " + ex + " is not executable")
+            else:
+                exe_file_valid = None
+                for path in os.environ["PATH"].split(os.pathsep):
+                    exe_file = os.path.join(path, ex)
+                    if os.path.isfile(exe_file) and __is_exe__(exe_file):
+                        exe_file_valid = exe_file
+                if exe_file_valid:
+                    print(" ".join(["Found", key, "in", exe_file_valid]))
+                else:
+                    raise EnvironmentError(key + ": " + ex + " is not executable or found in PATH")
+                
+                
 # main function
 if __name__ == '__main__':
     unittest.main()
