f97d06535960f28d76fb3a171be2bc69416ef328
Pay Giesselmann <pay.giesselmann@web.de>
2019-03-11 18:52:25 +0100

    add header and license

--- setup.py
+++ setup.py
@@ -9,10 +9,28 @@
 #  REQUIRES      : none
 #
 # -----------------------------------------------------------------------
-#  All rights reserved to Max Planck Institute for Molecular Genetics
-#  Berlin, Germany
-#  Written by Pay Giesselmann
-# -----------------------------------------------------------------------
+# Copyright (c) 2018-2019, Pay Giesselmann, Max Planck Institute for Molecular Genetics
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+#
+# Written by Pay Giesselmann
+# ---------------------------------------------------------------------------------
 import os, sys, shutil
 import site
 from setuptools import setup
@@ -43,7 +61,7 @@ class CustomInstallCmd(install):
 
 setup(
     name='nanopype',
-    version='0.3.0',
+    version='0.4.0',
     author='Pay Giesselmann',
     author_email='giesselmann@molgen.mpg.de',
     description='Nanopore data processing workflows',
